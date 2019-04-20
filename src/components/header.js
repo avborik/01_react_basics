7cc1b9d250e4b8ff9913f5db15553d527d424f02
AvBorikDeveloper <avboris28@gmail.com>
2019-04-20 23:30:25 +1200

    Using dynamic data

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,17 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-
+const user = {
+    name: 'Boris',
+    lastname: 'Avdeev',
+    age:28
+}
 const Header = () => {
-    return <div>This is my new Header</div>
+    return <div>
+        <div>{user.name}</div>
+        <div>{user.lastname}</div> 
+        <div>{user.age}</div>  
+        
+    </div>
 }
 
 export default Header;
